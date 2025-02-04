15c8fc6f740ad7f9207c995b87b2c7e96e06bc41
Sam Muldrow <smuldrow117@gmail.com>
2025-02-04 02:28:52 +0000

    Add DD docs

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,41 @@
 import streamlit as st
-from openai import OpenAI
+from llama_index.core import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core.node_parser import SentenceSplitter
+import openai
+from llama_index.core import SimpleDirectoryReader
+from llama_index.core import Settings
+
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Chat with Datadog Docs")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
+    "This is a chatbot that allows you to ask questions about the [Datadog Documentation](https://docs.datadoghq.com/) \n"
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the Datadog docs – hang tight! This should take 5-10 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        Settings.llm = OpenAI(
+            model="gpt-3.5-turbo",
+            temperature=0.2,
+            system_prompt="""You are an expert on 
+            the Datadog Documentation and your 
+            job is to answer technical questions. 
+            Assume that all questions are related 
+            to the Datadog documentation. Keep 
+            your answers technical and based on 
+            facts – do not hallucinate features. If something is not supported say that.""",
+        )
+        index = VectorStoreIndex.from_documents(docs)
+        return index
+
+
+
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
@@ -16,41 +44,28 @@ openai_api_key = st.text_input("OpenAI API Key", type="password")
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
+    openai.api_key = openai_api_key
+    index = load_data()
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
-
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
+    if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(
+            chat_mode="condense_question", verbose=True, streaming=True
         )
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
+    if prompt := st.chat_input(
+        "Ask a question about Datadog Docs!"
+    ):  # Prompt for user input and save to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+    for message in st.session_state.messages:  # Write message history to UI
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    # If last message is not from assistant, generate a new response
+    if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            response_stream = st.session_state.chat_engine.stream_chat(prompt)
+            st.write_stream(response_stream.response_gen)
+            message = {"role": "assistant", "content": response_stream.response}
+            # Add response to message history
+            st.session_state.messages.append(message)
